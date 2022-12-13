46ad9ab51851b657a40a484ff9db607a09db215c
Gabrielll04 <107888562+Gabrielll04@users.noreply.github.com>
2022-12-13 15:26:47 -0300

    Update app.js

--- app.js
+++ app.js
@@ -7,12 +7,13 @@ const passport = require('passport')
 require('dotenv').config()
 const app = express()
 
-//SequelizeStorage.sync({forced:true})
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
 
 app.use(session({
     secret: process.env.SESSION_SECRET,
     resave: false,
-    saveUninitialize: true,
+    saveUninitialized: true,
     store: sessionStorage.SequelizeStorage,
     cookie: {
         maxAge: 1024 * 60 * 60 * 24
@@ -26,6 +27,10 @@ app.use(passport.initialize())//initializing passport middleware
 app.use(passport.session())//has to do a little bit with serialize and deserialize
 //the express session gives us access to the request.session object and anything that we store on the request.json object inside any of the routes is going to be persisted to the database under the sessions collection
 
+app.use((req, res, next) => {
+    console.log(req.session)
+    console.log(req.user)
+})
 
 //routes
 app.use(pages)
