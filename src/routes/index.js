7210ab42ae89bc2d06d3c1a748f92ad65bca0499
Gabrielll04 <107888562+Gabrielll04@users.noreply.github.com>
2022-12-14 12:29:57 -0300

    Update index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,17 +1,23 @@
 const express = require('express')
 const passport = require('passport')
-const genPassword = require('../lib/passwordUtils')//in routes you must use genPassword, at the passport.js you use verifyPassword
+const utils = require('../lib/passwordUtils')//in routes you must use genPassword, at the passport.js you use verifyPassword
 const userTB = require('../model/User')
 const path = require('path')
 
 const router = express.Router()
 
 
-router.post('/login', passport.authenticate('local'), (req, res, next) => {})
+router.post('/login', passport.authenticate('local', {failureRedirect: '/failureLogin', successRedirect: '/homepage'}))
+
+router.get('/failureLogin', (req, res) => {res.send(404)})
+
+router.get('/login', (req, res) => {
+    res.sendFile(path.resolve('C:/Users/Aluno/Desktop/excel/FFC-AUTH/pages/loginForm.html'))
+})
 
 router.post('/register', (req, res, next) => {
 
-     const saltHash = genPassword(req.body.pw)
+     const saltHash = utils.genPassword(req.body.pw)
 
      const salt = saltHash.salt
      const hash = saltHash.genHash
@@ -24,19 +30,23 @@ router.post('/register', (req, res, next) => {
      .catch((err) => {console.log(err)})
 })
 
-router.get('/register', (req, res) => {
-    res.sendFile(path.resolve('C:/Users/Aluno/Desktop/excel/FFC-AUTH/pages/registerForm.html'));
-})
-
-router.get('/', (req, res) => {
-
+router.get('/homepage', (req, res) => {
+    
     if(req.session.viewCount){
         req.session.viewCount++
     }else {
         req.session.viewCount = 1
     }
 
-    res.send(`You have visited this website: ${req.session.viewCount} times. <a href="/register">Click hear to register</a>`)
+    res.send(`Você se logou. Bem vindo, ${req.user.User_name}. You have visited this website: ${req.session.viewCount} times. `)//In this form we get the user data
+})
+
+router.get('/register', (req, res) => {
+    res.sendFile(path.resolve('C:/Users/Aluno/Desktop/excel/FFC-AUTH/pages/registerForm.html'))
+})
+
+router.get('/', (req, res) => {
+    res.send(`<a href="/register">Click hear to register</a> and <a href="/login">hear to login</a>`)
 })
 
 module.exports = router
